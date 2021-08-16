6543ca3e631b10a31b582ad44ce65e7909a80bd0
EMG <dlghrms1992@naver.com>
2021-08-16 22:06:04 +0900

    ObserverPatter End~!

---
+++ DesignPattern/src/OpserverPattern/exercise/ForecastDisplay.java
@@ -0,0 +1,30 @@
+package OpserverPattern.exercise;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import OpserverPattern.weather.DisplayElement;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+
+	private float currentPressure = 29.95f;
+	private float lastPressure;
+	
+	public ForecastDisplay(Observable observable) {
+		observable.addObserver(this);
+	}
+	
+	public void update(Observable observable, Object arg) {
+		if(observable instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData)observable;
+			lastPressure = currentPressure;
+			currentPressure = weatherData.getPressure();
+			display();
+		}
+	}
+	
+	public void display() {
+		System.out.println("화면 변경");
+	}
+	
+}
