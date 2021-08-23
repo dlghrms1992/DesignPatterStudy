e24d38d1dc016b5226fe426bbc84b91ff196c76d
EMG <dlghrms1992@naver.com>
2021-08-23 22:26:43 +0900

    FactoryPattern Real End~!

--- DesignPattern/src/FactoryPattern/pizza/ChicagoPizzaStore.java
+++ DesignPattern/src/FactoryPattern/pizza/ChicagoPizzaStore.java
@@ -1,5 +1,23 @@
 package FactoryPattern.pizza;
 // 서브클래스 지역마다 다른 피자스타일을 구현하기 위해서
-public class ChicagoPizzaStore {
+public class ChicagoPizzaStore extends PizzaStore {
 
+	
+	
+	public ChicagoPizzaStore(SimplePizzaFactory factory) {
+		super(factory);
+	}
+
+	@Override
+	public Pizza orderPizza(String type) {
+		return super.orderPizza(type);
+	}
+
+	@Override
+	Pizza createPizza(String type) {
+		return null;
+	}
+
+	
+	
 }
